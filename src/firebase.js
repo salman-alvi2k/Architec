acdab0a16a21807a41c8ec16f967a42ed10097e8
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-08-07 18:46:41 +0500

    feat: added firbase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
-import 'firebase/compat/storage';
-
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/storage";
 
 // Import the functions you need from the SDKs you need
 
@@ -12,13 +11,13 @@ import 'firebase/compat/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB2nBQ1fdRezEvtQW3uf2Lj4Z3F6su0Whk",
-  authDomain: "architec-7dd1f.firebaseapp.com",
-  projectId: "architec-7dd1f",
-  storageBucket: "architec-7dd1f.appspot.com",
-  messagingSenderId: "759108364735",
-  appId: "1:759108364735:web:ab02dbc2c7ba441aa4190a",
-  measurementId: "G-8TKZE5GP6R"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -26,8 +25,5 @@ const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore(app);
 const auth = firebase.auth(app);
 const storage = firebase.storage(app);
-
-
-
 
 export { db, auth, storage };
